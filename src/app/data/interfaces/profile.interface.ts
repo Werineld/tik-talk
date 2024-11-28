24a794723b99b39172981a9f6064e7d01552e194
Werineld <beyondreachrastibulcka@gmail.com>
2024-11-28 15:30:45 +0300

    modified:   angular.json 	modified:   src/app/app.component.html 	modified:   src/app/app.component.scss 	modified:   src/app/app.component.ts 	modified:   src/app/app.config.ts 	new file:   src/app/common-ui/profile-card/profile-card.component.html 	new file:   src/app/common-ui/profile-card/profile-card.component.scss 	new file:   src/app/common-ui/profile-card/profile-card.component.ts 	modified:   src/app/data/interfaces/profile.interface.ts 	new file:   src/app/data/services/profile.service.ts 	new file:   src/app/helpers/pipes/img-url.pipe.ts 	modified:   src/fonts.scss 	modified:   src/styles.scss

---
+++ src/app/data/interfaces/profile.interface.ts
@@ -0,0 +1,13 @@
+export interface Profile {
+  id: number,
+  username: string,
+  description: string,
+  avatarUrl: string | null,
+  subscribersAmount: number,
+  firstName: string,
+  lastName: string,
+  isActive: boolean,
+  stack: string[],
+  city: string | null,
+}
+
